0ff17c1ddca40b898026ad497dc48de95511b2e7
Luiz Fernando <fernandoluizalves24@gmail.com>
2026-01-22 00:15:34 -0300

    atualizando pag inicial
    bloco de anuncios finalizado

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var swiper = new Swiper(".mySwiper", {
-      spaceBetween: 30,
-      effect: "fade",
-      navigation: {
+    spaceBetween: 30,
+    effect: "fade",
+    autoplay: {
+        delay: 5000, // tempo em ms (5 segundos)
+        disableOnInteraction: false, // continua mesmo se clicar
+    },
+    navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-      },
-      pagination: {
+    },
+    pagination: {
         el: ".swiper-pagination",
         clickable: true,
-      },
-    });
+    },
+});
